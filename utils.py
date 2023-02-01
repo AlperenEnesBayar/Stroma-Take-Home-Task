6164ec89e37df3231d995fb2f062b2a64e19d921
AlperenEnesBayar <45437411+AlperenEnesBayar@users.noreply.github.com>
2023-02-01 09:04:33 +0300

    Functions are improved.
    - Comments are added.

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import os
 import cv2
+import json
 from glob import glob
 
 
-# Generates a name based on the image's number.
+# Generates a name based on the image's id.
 def fileNameGenerator(counter, name_lenght=4, extension="jpg"):
     image_name = ""
     for _ in range(name_lenght - len(str(counter))):
@@ -21,7 +22,7 @@ def videoExtractor(video_path, output_folder_path):
     if os.path.exists(video_path):
         vidcap = cv2.VideoCapture(video_path)
     else:
-        raise "Image not found. Please check the path"
+        raise "Video not found. Please check the path"
 
     # If output folder not exists create a new directory
     if not os.path.exists(output_folder_path):
@@ -38,9 +39,59 @@ def videoExtractor(video_path, output_folder_path):
 
 
 # Finds all the videos in the specified data file and extracts their images. Then it deletes the videos.
-def extractAllVideos():
-    video_paths = glob("data\\images\\*\\*.mp4")
+def extractAllVideos(dataFileSystem):  # Ex: "data\\images\\*\\*.mp4"
+    video_paths = glob(dataFileSystem)
     for video_path in video_paths:
         folder_in_path = video_path.split('\\')[:-1]
         videoExtractor(video_path, folder_in_path[0] + "\\" + folder_in_path[1] + "\\" + folder_in_path[2])
         os.remove(video_path)
+
+
+def instance2Yolov7Label(label_path, output_folder_path, w_img=640, h_img=640):
+    if os.path.exists(label_path):
+        f = open(label_path)
+        data = json.load(f)
+        f.close()  # Closing file
+
+        # if it doesn’t exist we create one
+        if not os.path.exists(output_folder_path):
+            os.makedirs(output_folder_path)
+
+        # Iterating through the json
+        for single_annotation in data['annotations']:
+            text_file_name = fileNameGenerator(single_annotation['image_id'], extension='txt')
+            f = open(output_folder_path + "\\" + text_file_name, "a")
+
+            id = '0' if single_annotation['category_id'] == 1 else '1'
+
+            w = single_annotation['bbox'][2]
+            h = single_annotation['bbox'][3]
+            xcenter = (single_annotation['bbox'][0] + w / 2) / w_img
+            ycenter = (single_annotation['bbox'][1] + h / 2) / w_img
+            w = w / w_img
+            h = h / h_img
+
+            f.write(id + " " +
+                    str(xcenter) + " " +
+                    str(ycenter) + " " +
+                    str(w) + " " +
+                    str(h) + '\n')
+            f.close()
+
+        for file in glob(output_folder_path + "\\*.txt"):
+            with open(file, 'r+') as fp:
+                lines = fp.readlines()
+                fp.seek(0)
+                fp.truncate()
+                lines[-1] = lines[-1].replace("\n", "")
+                fp.writelines(lines)
+
+    else:
+        raise "Label not found. Please check the path"
+
+
+def convertYolov7Labels():
+    for pa in ['train', 'test', 'val']:
+        instance2Yolov7Label("data/labels/instances_" + pa + ".json", "data/annotations/" + pa)
+
+convertYolov7Labels()
